7f53f542444984cee6b017123388336858a56c34
Mosdevv <mosDev2017@gmail.com>
2018-02-21 17:49:34 -0500

    setup

--- GenericPhotoApp/MainViewController.swift
+++ GenericPhotoApp/MainViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
